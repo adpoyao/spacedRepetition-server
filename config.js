f4d4fbe84f6bd0be9d72ba52ff23cd0be952db1a
Nate Garrett <nate.garrett1992@gmail.com>
2018-01-08 16:09:45 -0600

    testing heroku bug

--- config.js
+++ config.js
@@ -4,9 +4,10 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-      process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+  CLIENT_ORIGIN: 'https://kurikaeshi.netlify.com',
+  // process.env.CLIENT_ORIGIN || 'http://localhost:3000' || ,
+  DATABASE_URL: 'mongodb://dev:dev@ds247587.mlab.com:47587/kurikaeshi',
+      // process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
   TEST_DATABASE_URL:
       process.env.TEST_DATABASE_URL ||
       'mongodb://localhost/thinkful-backend-test',
