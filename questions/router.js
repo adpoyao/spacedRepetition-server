d15bbcf96f664bb38996d7c91adf1c17be926b87
Andy Gaines <andyagaines@gmail.com>
2018-01-09 11:22:19 -0600

    finished delete

--- questions/router.js
+++ questions/router.js
@@ -86,3 +86,10 @@ router.put('/:id', (req, res) => {
 		.then(question => res.status(204).end())
 		.catch(err => res.status(500).json({message: 'Internal server error'}));
 });
+
+router.delete('/:id', (req, res) => {
+	Question
+		.findByIdAndRemove(req.params.id)
+		.then(() => res.status(204).end())
+		.catch(err => res.status(500).json({message: 'Internal server error'}));
+});
